5967c99d77b3e5f275465fd44d70ebabe212b83a
home <413500023@qq.com>
2022-08-13 23:11:00 +0900

    8/13 p.111

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -6,17 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
 
 @Slf4j
 @Controller
-@RequestMapping("/orders")
+@RequestMapping("/orders") //处理路径以“/orders”开头的请求
 public class OrderController {
-    @GetMapping("/current")
+    @GetMapping("/current") //指定orderForm方法，处理针对“/orders/current”的HTTP GET请求
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 }
