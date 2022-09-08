b53e24a7d8005f41f1eab09f214a8498ae49e351
wang0902 <413500023@qq.com>
2022-09-08 18:40:21 +0900

    8.30 p.155

--- src/main/java/tacos/security/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package tacos.security;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-    //loadUserByUsername()方法接受用户名使用它查找UserDetails对象，找不到用户，则抛出错误
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
